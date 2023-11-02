e6d02652372d2fcd06c6f38335aa6d164ba9e588
Essi Savonius <essi.savonius@hotmail.fi>
2023-11-02 18:13:31 +0200

    varasto.py testikattavuus 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,33 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    # laajennus
+
+    def test_negatiivinen_tilavuus_asettaa_nollaksi(self):
+        varasto = Varasto(-1)
+        self.assertEqual(varasto.tilavuus, 0.0)
+    
+    def test_saldo_negatiivinen_nollataan(self):
+        varasto = Varasto(10, -1)
+        self.assertEqual(varasto.saldo, 0.0)
+    
+    def test_alkusaldo_tilavuutta_suurempi_asettaa_tilavuudeksi(self):
+        varasto = Varasto(10, 20)
+        self.assertEqual(varasto.saldo, 10)
+    
+    def test_lisaaminen_alle_nolla_maaralla_palauttaa_none(self):
+        self.assertEqual(self.varasto.lisaa_varastoon(-1), None)
+    
+    def test_lisays_ei_mahdu_varastoon(self):
+        self.varasto.lisaa_varastoon(11)
+        self.assertEqual(self.varasto.saldo, self.varasto.tilavuus)
+    
+    def test_palauttaa_nolla_jos_ottaa_alle_nolla_maaralla(self):
+        self.assertEqual(self.varasto.ota_varastosta(-1), 0.0)
+    
+    def test_ottaminen_saldoa_suuremmalla_palauttaa_saldon(self):
+        self.assertEqual(self.varasto.ota_varastosta(10), 0.0)
+    
+    def test_str_palauttaa_oikean_arvon(self):
+        self.assertEqual(str(self.varasto), f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}")
